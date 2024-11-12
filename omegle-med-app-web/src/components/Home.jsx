928aa1d3c8535d2616a1d7759ffda075fb1aa589
Julieta Bulfon <j.bulfon@itecriocuarto.org.ar>
2024-11-12 19:39:38 -0300

    changes in css home

--- omegle-med-app-web/src/components/Home.jsx
+++ omegle-med-app-web/src/components/Home.jsx
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react';
 import styles from './Home.module.css';
 import { useNavigate } from 'react-router-dom';
-import { AuthContext } from '../context/AuthContext'; // Importa el contexto de autenticación
+import { AuthContext } from '../context/AuthContext';
 
 const Home = () => {
-  const { user } = useContext(AuthContext); // Obtén el usuario desde el contexto
+  const { user } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const goDocsList = () => {
@@ -15,30 +15,30 @@ const Home = () => {
     if (user?.role === 'user') {
       navigate('/appointments');
     } else if (user?.role === 'admin') {
-        navigate(`/appointments`);
+      navigate(`/appointments`);
     } else if (user?.role === 'doctor') {
       navigate(`/doctor/appointments/${user.id}`);
     } else {
-      navigate('/login'); // Si no está autenticado, redirigir a login
+      navigate('/login');
     }
   };
 
   return (
     <div>
+      <h1 className={styles.instituteName}>Instituto Médico Omegle</h1> {/* Título para el nombre del instituto */}
       <div className={styles.sloganContainer}>
         <button onClick={goDocsList} className={styles.sloganCard}>
-          <p>Nuestros Profesionales</p>
+          <span role="img" aria-label="doctor" className={styles.icon}>👨‍⚕️</span>
+          <p>Lista de Profesionales</p>
         </button>
         <button onClick={goToAppointments} className={styles.sloganCard}>
-          <p>Reserva tu turno en segundos</p>
-        </button>
-
-        <button className={styles.sloganCard}>
-          <p>Confianza y calidad en cada consulta</p>
+          <span role="img" aria-label="calendar" className={styles.icon}>📅</span>
+          <p>Reserva tu turno</p>
         </button>
       </div>
     </div>
   );
+  
 };
 
 export default Home;
